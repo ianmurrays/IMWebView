6d06211bf07f001fd43463c5bf768707824c98db
Ian Murray <ianmurrays@gmail.com>
2013-01-04 10:24:20 -0300

    WIP: changed how the API works

--- IMWebView/IMWebView.h
+++ IMWebView/IMWebView.h
@@ -21,18 +21,43 @@ typedef void (^IMWebViewCallback)();
  @param url The url to navigate to
  @param callback Block to be invoked when the page is ready to be interacted with.
  */
-- (void)goToURL:(NSURL *)url withCallback:(IMWebViewCallback)callback;
+- (void)goToURL:(NSURL *)url withCallback:(IMWebViewCallback)callback DEPRECATED_ATTRIBUTE;
 
 // TODO: Document these methods!
-- (void)goForwardWithCallback:(IMWebViewCallback)callback;
-- (void)goBackWithCallback:(IMWebViewCallback)callback;
-- (void)stop;
-- (void)reloadWithCallback:(IMWebViewCallback)callback;
-- (BOOL)clickElementWithSelector:(NSString *)selector withCallback:(IMWebViewCallback)callback;
-- (BOOL)clickLabel:(NSString *)label withCallback:(IMWebViewCallback)callback;
-- (BOOL)clickLabel:(NSString *)label withTag:(NSString *)tag withCallback:(IMWebViewCallback)callback;
+
+/**
+ Starts a new request and calls the callback when the request is done.
+ 
+ @param url The url from where to start
+ @param callback The callback to invoke when the DOM is ready.
+ */
+- (void)startWithURL:(NSURL *)url withCallback:(IMWebViewCallback)callback;
+
+/**
+ You can call this consecutively to enqueue operations. You should call this
+ after having performed an operation that makes the internal web view load another
+ site.
+ 
+ @param callback The callback to invoke when the DOM is ready.
+ */
+- (void)thenExecuteBlock:(IMWebViewCallback)callback;
+
+/**
+ Call this when all your operations have been enqueued. This will
+ run them all and call the callback when finished.
+ 
+ @param callback Invoked when all operations have finished executing.
+ */
+- (void)runWithCallback:(IMWebViewCallback)callback;
+
+
+- (void)goForward;
+- (void)goBack;
+- (BOOL)clickElementWithSelector:(NSString *)selector;
+//- (BOOL)clickLabel:(NSString *)label;
+//- (BOOL)clickLabel:(NSString *)label withTag:(NSString *)tag;
 - (BOOL)fillFieldWithSelector:(NSString *)selector withValue:(NSString *)string;
-- (BOOL)fillFormWithSelector:(NSString *)selector options:(NSDictionary *)options submit:(BOOL)submit withCallback:(IMWebViewCallback)callback;
+- (BOOL)fillFormWithSelector:(NSString *)selector options:(NSDictionary *)options submit:(BOOL)submit;
 - (NSString *)getHTMLInSelector:(NSString *)selector;
 
 @end
--- IMWebView/IMWebView.m
+++ IMWebView/IMWebView.m
@@ -8,11 +8,25 @@
 
 #import "IMWebView.h"
 
+typedef enum
+{
+    IMWebViewOperationNavigate,
+    IMWebViewOperationExecuteBlock,
+    IMWebViewOperationRun
+} IMWebViewOperation;
+
 @interface IMWebView () <UIWebViewDelegate>
 
 @property (nonatomic,strong) UIWebView *webView;
 
 @property (nonatomic,copy) IMWebViewCallback callbackBlock;
+
+/**
+ Unlike what the name might suggest, this holds a list of operations,
+ (as in open this page, then click this link, then fill this form), 
+ in order to perform them when calling -run.
+ */
+@property (nonatomic,strong) NSMutableArray *operationsQueue;
 
 @end
 
@@ -36,16 +50,76 @@
     self.webView.delegate = nil;
 }
 
+/**
+ Enqueues an operation into the operationsQueue to be run later
+ 
+ @param operation The operation type
+ @param options The necessary options to run the operation
+ */
+- (void)enqueueOperation:(IMWebViewOperation)operation withOptions:(NSDictionary *)options
+{
+    [self.operationsQueue addObject:@{@"operation" : @(operation), @"options" : options}];
+}
+
 #pragma mark - Navigation
 
-- (void)goToURL:(NSURL *)url withCallback:(IMWebViewCallback)callback
+- (void)goToURL:(NSURL *)url withCallback:(IMWebViewCallback)callback DEPRECATED_ATTRIBUTE
 {
     self.callbackBlock = callback;
     
     [self.webView loadRequest:[NSURLRequest requestWithURL:url]];
 }
 
-#pragma mark - DOM Manipulation / Interaction
+- (void)startWithURL:(NSURL *)url withCallback:(IMWebViewCallback)callback
+{
+    [self enqueueOperation:IMWebViewOperationNavigate withOptions:@{@"url" : url}];
+}
+
+- (void)thenExecuteBlock:(IMWebViewCallback)callback
+{
+    [self enqueueOperation:IMWebViewOperationExecuteBlock withOptions:@{@"block" : [callback copy]}];
+}
+
+- (void)runWithCallback:(IMWebViewCallback)callback
+{
+    [self enqueueOperation:IMWebViewOperationRun withOptions:@{@"callback" : [callback copy]}];
+}
+
+#pragma mark - Public DOM Manipulation / Interaction Methods
+
+- (BOOL)selectorExists:(NSString *)selector
+{
+    NSString *stringToEvaluate = [NSString stringWithFormat:@"$imWebViewJquery('%@').length;", selector];
+    NSString *response = [self.webView stringByEvaluatingJavaScriptFromString:stringToEvaluate];
+    
+    return response.integerValue > 0;
+}
+
+- (BOOL)clickElementWithSelector:(NSString *)selector
+{
+    if ([self selectorExists:selector])
+    {
+        NSString *stringToEvaluate = [NSString stringWithFormat:@"$imWebViewJquery('%@')[0].click();", selector];
+        [self.webView stringByEvaluatingJavaScriptFromString:stringToEvaluate];
+        
+        return YES;
+    }
+    
+    return NO;
+}
+
+- (BOOL)fillFieldWithSelector:(NSString *)selector withValue:(NSString *)string
+{
+    if ([self selectorExists:selector])
+    {
+        // FIXME: Need to escape `string`
+        NSString *stringToEvaluate = [NSString stringWithFormat:@"$imWebViewJquery('%@').val('%@');", selector, string];
+    }
+    
+    return NO;
+}
+
+#pragma mark - Private DOM Manipulation / Interaction Methods
 
 - (void)injectScriptWithName:(NSString *)name
 {
